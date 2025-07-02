03f807110c5cfbb5acc4bb6b0ec9b90a25e3604c
Shrimanthv <shrimanthv99@gmail.com>
2025-07-02 17:24:13 +0530

    Data Ingestion Completed.

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.Text_Tamer.pipeline.step_1_data_ingestion import DataIngestionTrainingPipeline 
+from src.Text_Tamer.logging import logger
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
